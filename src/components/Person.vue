9fca03844f845d1de49ab95383ce3a8bcd1c5db3
CyberLelouch <167730646@qq.com>
2024-09-03 09:58:47 +0800

    002-toRefs(Obj)修改汽车品牌

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,20 +2,24 @@
     <div class="person">
         <h2>这是一俩{{car.brand}}车，价值{{car.price}}万</h2>
         <button @click="changePrice">修改价格</button>
+        <button @click="changeBrand">修改品牌</button>
     </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, toRefs } from 'vue';
 
     let car = reactive({
         brand:'bmw',
         price:100
     });
+    let {brand,price} = toRefs(car);
     function changePrice(){
         car.price += 10;
     }
-    
+    function changeBrand(){
+        brand.value = 'BYD'
+    }
 </script>
 
 <style scoped>
