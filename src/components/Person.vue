7245ca13541699c0e826e7e69507db50d9c3aacd
CyberLelouch <167730646@qq.com>
2024-09-03 09:36:44 +0800

    00-init

---
+++ src/components/Person.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="person"></div>
+</template>
+
+<script setup lang="ts">
+    let car = 
+</script>
+
+<style scoped>
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
