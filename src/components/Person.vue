f6370058c314d559c88d977736b1862d43904e86
CyberLelouch <167730646@qq.com>
2024-09-03 09:51:27 +0800

    001-reactive修改汽车价格

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="person">
-        <h2></h2>
+        <h2>这是一俩{{car.brand}}车，价值{{car.price}}万</h2>
+        <button @click="changePrice">修改价格</button>
     </div>
 </template>
 
@@ -10,7 +11,11 @@ import { reactive } from 'vue';
     let car = reactive({
         brand:'bmw',
         price:100
-    })
+    });
+    function changePrice(){
+        car.price += 10;
+    }
+    
 </script>
 
 <style scoped>
