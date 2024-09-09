371df1fd1ae013bd78819539cb624a64f2b1b53b
CyberLelouch <167730646@qq.com>
2024-09-09 18:58:48 +0800

    005-将RouterLink 目标位置改为对象类型{path:'/home'}

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
         <Header></Header>
         <!-- 导航区 -->
          <div class="navigate">
-            <RouterLink to="/home" active-class="active">首页</RouterLink>
-            <RouterLink to="/news" active-class="active">新闻</RouterLink>
-            <RouterLink to="/about" active-class="active">关于</RouterLink>
+            <RouterLink :to="{path:'/home'}" active-class="active">首页</RouterLink>
+            <RouterLink :to="{path:'/news'}" active-class="active">新闻</RouterLink>
+            <RouterLink :to="{path:'/about'}" active-class="active">关于</RouterLink>
          </div>
 
          <!-- 展示区域 -->
