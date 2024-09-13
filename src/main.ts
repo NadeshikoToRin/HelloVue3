417fd5c2149f73b437c544b4c77850360a9b651a
CyberLelouch <167730646@qq.com>
2024-09-13 23:16:10 +0800

    012-为pinia准备一个效果

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
-// 引入createApp用于创建应用
-import {createApp} from 'vue'
-// 引用App根组件
+import { createApp } from "vue";
 import App from './App.vue'
-// 引入router
-import router from './router'
 
-// 创建一个app
-const app = createApp(App)
-// 使用router
-app.use(router);
-// 将app挂载到页面上
+//创建一个应用
+const app = createApp(App);
+
 app.mount('#app')
