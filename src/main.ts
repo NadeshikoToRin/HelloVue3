cdce67ec280f907aee76bef858dbd27eacf5ee78
CyberLelouch <167730646@qq.com>
2024-09-17 16:04:14 +0800

    013-搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from "vue";
 import App from './App.vue'
 
+//第一步：引入pinia
+import { createPinia } from "pinia";
+
 //创建一个应用
 const app = createApp(App);
 
+//第二步：创建pinia
+const pinia = createPinia();
+
+//第三步：使用pinia
+app.use(pinia);
 app.mount('#app')
