4214141204a9e379dc75c38636174e5298021088
CyberLelouch <167730646@qq.com>
2024-09-17 19:50:21 +0800

    018-将store.ts改成组件式

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,28 +1,55 @@
 
 import { defineStore } from 'pinia'
+import { computed, reactive, ref } from 'vue';
 
-export const useCountStore = defineStore('count', {
+// export const useCountStore = defineStore('count', {
 
-    actions: {
-        increment(value:number){
-            if(this.sum < 10){
-                this.sum += value;
-            }
-            console.log("increment被调用了",this);
+//     actions: {
+//         increment(value:number){
+//             if(this.sum < 10){
+//                 this.sum += value;
+//             }
+//             console.log("increment被调用了",this);
             
+//         }
+//     },
+//     // 真正存储数据的地方
+//     state() {
+//         return {
+//             sum: 6,
+//             address: "xi'an"
+//         }
+//     },
+//     getters:{
+//         tenTimesSum:state => state.sum * 10,
+//         upperAddress():string{
+//             return this.address.toUpperCase();
+//         }
+//     }
+// })
+
+export const useCountStore = defineStore('count', ()=>{
+    let sum = ref(1);
+    let address = ref("xi'an");
+    let tenTimesSum = computed({
+        get(){
+            return sum.value * 10;
+        },
+        set(value){
+
         }
-    },
-    // 真正存储数据的地方
-    state() {
-        return {
-            sum: 6,
-            address: "xi'an"
+    })
+    let upperAddress = computed({
+        get(){
+            return address.value.toUpperCase();
+        },
+        set(value){
+
         }
-    },
-    getters:{
-        tenTimesSum:state => state.sum * 10,
-        upperAddress():string{
-            return this.address.toUpperCase();
-        }
+    })
+
+    function increment(num: number){
+        sum.value += num;
     }
+    return {sum ,address,tenTimesSum,upperAddress,increment}
 })
--- src/store/loveTalk.ts
+++ src/store/loveTalk.ts
@@ -4,20 +4,40 @@ import { defineStore } from "pinia";
 import axios from "axios"
 import { nanoid } from 'nanoid'
 
-//创建stroe并暴露
-export const useTalkStore = defineStore('talk', {
+// //创建stroe并暴露
+// export const useTalkStore = defineStore('talk', {
 
-    actions: {
-        async getTalk() {
-            let { data: { content: title } } = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
-            let obj = { id: nanoid(), title }
-            this.talkList.unshift(obj);
-        }
-    },
+//     actions: {
+//         async getTalk() {
+//             let { data: { content: title } } = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
+//             let obj = { id: nanoid(), title }
+//             this.talkList.unshift(obj);
+//         }
+//     },
 
-    state() {
-        return {
-            talkList: JSON.parse(localStorage.getItem('talkList') as string) || []
-        }
-    }
+//     state() {
+//         return {
+//             talkList: JSON.parse(localStorage.getItem('talkList') as string) || []
+//         }
+//     }
+// })
+
+import { reactive } from "vue";
+
+export const useTalkStore = defineStore('talk',()=>{
+    // talkList就是state
+  const talkList = reactive(
+    JSON.parse(localStorage.getItem('talkList') as string) || []
+  )
+
+  // getATalk函数相当于action
+  async function getTalk(){
+    // 发请求，下面这行的写法是：连续解构赋值+重命名
+    let {data:{content:title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
+    // 把请求回来的字符串，包装成一个对象
+    let obj = {id:nanoid(),title}
+    // 放到数组中
+    talkList.unshift(obj)
+  }
+  return {talkList,getTalk}
 })
